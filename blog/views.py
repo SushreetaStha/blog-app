6012970898ed0a852189a28527853587853425c0
Sushreeta Shrestha <sushreetashrestha@gmail.com>
2023-03-31 11:11:55 +0545

    update

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,21 @@ from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import Blog
 from django.urls import reverse_lazy
 from . import forms
+from django.db.models import Q
 
 class BlogList(ListView):
     model= Blog
     template_name='blog/home.html'
-    # queryset=Blog.objects.all().order_by('-created_at')
+    queryset=Blog.objects.all().order_by('-created_at')
     paginate_by=2
+
+    def get_queryset(self):
+        q=self.request.GET.get('q')
+        if q:
+            object_list=self.model.objects.filter(title__icontains=q)
+        else:
+            object_list=self.model.objects.all()
+        return object_list    
 
 class BlogCreate(CreateView):
     model=Blog
